a4f9c31e1cec1992ad37eaace3942bcf58325138
Jovany Leandro G.C <bit4bit@riseup.net>
2026-06-01 16:41:46 +0300

    jphone not working

---
+++ jphone/project.clj
@@ -0,0 +1,18 @@
+(defproject jphone "0.1-SNAPSHOT"
+  :license {:name "MPL 2.0"
+            :url "https://www.mozilla.org/en-US/MPL/2.0/"}
+  :dependencies []
+  :plugins [[org.jank-lang/lein-jank "0.6"]]
+  :middleware [leiningen.jank/middleware]
+  :main jphone.main
+  :jank {
+         :include-dirs ["/usr/local/include"]
+         :library-dirs ["/usr/local/lib"]
+         :linked-libraries ["pjsua2" "pjsip" "pjmedia"]
+         }
+  :profiles {:base {:jank {:output-dir "target/debug"
+                           :optimization-level 0
+                           }}
+             :release {:jank {:output-dir "target/release"
+                              :optimization-level 2
+                              }}})
